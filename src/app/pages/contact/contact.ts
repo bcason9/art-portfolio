bccbf1b47ed4cd1d7f4e57e78b2a8dad6258f80b
Ben Cason <bencason9@gmail.com>
2026-06-15 20:25:01 -0400

    adding fixes for page routing

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })

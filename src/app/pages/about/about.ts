867ca3da096fb6f7e1cf05464a69e123b438bb95
Ben Cason <bencason9@gmail.com>
2026-06-19 16:26:07 -0400

    adds footer shared component

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar';
+import { FooterComponent } from '../../shared/footer/footer';
 
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
+
 export class About {}
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar';
+import { FooterComponent } from '../../shared/footer/footer';
 
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })

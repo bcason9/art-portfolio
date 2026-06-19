fca55146ba44e0dd651a3f20f7be650738546bad
Ben Cason <bencason9@gmail.com>
2026-06-19 15:55:48 -0400

    adds navbar shared component

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../../shared/navbar/navbar';
 
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../../shared/navbar/navbar';
 
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
